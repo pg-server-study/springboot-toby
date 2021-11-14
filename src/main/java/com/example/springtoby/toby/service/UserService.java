5816f1c996e1ec400c2816a6efdfcbf10c499e2a
pursue503 <pursue503@naver.com>
2021-11-14 23:26:45 +0900

    Fix UserService

--- src/main/java/com/example/springtoby/toby/service/UserService.java
+++ src/main/java/com/example/springtoby/toby/service/UserService.java
@@ -1,11 +1,11 @@
 package com.example.springtoby.toby.service;
 
 import com.example.springtoby.toby.User;
-import org.springframework.stereotype.Service;
 
 public interface UserService {
 
     void add(User user);
+
     void upgradeLevels();
 
 }
